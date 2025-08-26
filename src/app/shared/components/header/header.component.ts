5d021800562b0dd92e3ac07866b2f0a43f0597b8
kevin <kevin.krechan@hochfrequenz.de>
2025-08-26 08:48:52 +0200

    make header more responsive

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { IconLogoComponent } from '../icon-logo/icon-logo.component';
 import { LoginButtonComponent } from '../login-button/login-button.component';
@@ -6,7 +7,13 @@ import { LoginButtonComponent } from '../login-button/login-button.component';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, IconLogoComponent, LoginButtonComponent],
+  imports: [CommonModule, RouterLink, IconLogoComponent, LoginButtonComponent],
   templateUrl: './header.component.html',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  isMobileMenuOpen = signal(false);
+
+  toggleMobileMenu(): void {
+    this.isMobileMenuOpen.update(current => !current);
+  }
+}
