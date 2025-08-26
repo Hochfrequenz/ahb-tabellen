55ffcec582676b1a9004a99df3464b971b1d674d
kevin <kevin.krechan@hochfrequenz.de>
2025-08-26 10:10:51 +0200

    Improve header and put input fields in collapsable burger menu

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -3,12 +3,20 @@ import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { IconLogoComponent } from '../icon-logo/icon-logo.component';
 import { LoginButtonComponent } from '../login-button/login-button.component';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [CommonModule, RouterLink, IconLogoComponent, LoginButtonComponent],
   templateUrl: './header.component.html',
+  animations: [
+    trigger('expandCollapse', [
+      state('closed', style({ height: '0px', opacity: 0, overflow: 'hidden' })),
+      state('open', style({ height: '*', opacity: 1, overflow: 'hidden' })),
+      transition('closed <=> open', animate('200ms ease-in-out')),
+    ]),
+  ],
 })
 export class HeaderComponent {
   isMobileMenuOpen = signal(false);
