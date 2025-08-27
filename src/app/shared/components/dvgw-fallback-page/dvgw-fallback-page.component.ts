7fd5772b6908d9506e6c89575ad76da9cbb4ea34
kevin <kevin.krechan@hochfrequenz.de>
2025-08-27 13:24:24 +0200

    align fallback components

--- src/app/shared/components/dvgw-fallback-page/dvgw-fallback-page.component.ts
+++ src/app/shared/components/dvgw-fallback-page/dvgw-fallback-page.component.ts
@@ -9,5 +9,5 @@ import { CommonModule } from '@angular/common';
 })
 export class DvgwFallbackPageComponent {
   readonly message =
-    'Die Prüfidentifikatoren 70095 und 70096 werden von in einem PDF Dokument der DVGW veröffentlicht. Uns stehen zur Zeit nur die Dokumente des BDEW in maschinenlesbarer Form bereit.';
+    'Die Prüfidentifikatoren 70095 und 70096 werden in einem PDF Dokument der DVGW veröffentlicht. Uns stehen zur Zeit nur die Dokumente des BDEW in maschinenlesbarer Form bereit.';
 }
