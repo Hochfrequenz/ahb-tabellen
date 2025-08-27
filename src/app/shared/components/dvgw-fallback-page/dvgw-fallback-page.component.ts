c1238645518d52fd8addb9ec512ac0b084d0f797
kevin <68426071+hf-krechan@users.noreply.github.com>
2025-08-27 15:34:51 +0200

    feat(dvgw-fallback): Add DVGW Fallback Component (#649)
    * make landing page responsive
    
    * make ahb landing page responsive
    
    * add fallback page for dvgw pruefis
    
    * delete scss file from ahb-table component
    
    * remove style url in dvgw fallback page
    
    * fix ahb tabellen
    
    * add fallback page for dvgw pruefis
    
    * delete scss file from ahb-table component
    
    * remove style url in dvgw fallback page
    
    * fix ahb tabellen
    
    * add link to DVGW document
    
    * align fallback components
    
    * align fallback components part 2
    
    * declare const variables for pruefis
    
    * use computed property
    
    * use private readonly for memi prüfis

---
+++ src/app/shared/components/dvgw-fallback-page/dvgw-fallback-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-dvgw-fallback-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './dvgw-fallback-page.component.html',
+})
+export class DvgwFallbackPageComponent {
+  readonly message =
+    'Die Prüfidentifikatoren 70095 und 70096 werden in einem PDF Dokument der DVGW veröffentlicht. Uns stehen zur Zeit nur die Dokumente des BDEW in maschinenlesbarer Form bereit.';
+}
