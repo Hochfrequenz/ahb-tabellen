a347dc94dbb352717b4c5aaaea69512113ed9694
kevin <kevin.krechan@hochfrequenz.de>
2025-10-21 00:17:45 +0200

    Chore: Remove unused 'Inject' import

--- src/app/shared/components/feature-switcher/feature-switcher.component.ts
+++ src/app/shared/components/feature-switcher/feature-switcher.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, ElementRef, Inject } from '@angular/core';
+import { Component, HostListener, ElementRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 
